137e6a77b76d113e0b579b5ffa5dbd38a5ef717d
Diego Vitali <drochavitali@gmail.com>
2024-09-11 21:22:56 -0300

    Upload 0.0.1
    Ainda resta a pasta do ADMINLTE

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Consultoria de Transporte</title>
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="css\index.css">
+	<link rel="stylesheet" href="adminlte/plugins/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+  
+</head>
+
+<body class="layout-top-nav control-sidebar-slide-open dark-mode layout-navbar-fixed" style="height: auto;">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand-md navbar-gray navbar-dark">
+    <div class="container">
+      <a href="#" class="navbar-brand">
+        <img src="libraries/Logo FL.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light freteLog" style="font-weight: bold; letter-spacing: 2px; font-size: larger; padding-right: 10px; border-right: solid 1px white">Frete Log</span>
+      </a>
+
+      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav" style="font-weight: bold; letter-spacing: 2px; font-size: large">
+          <li class="nav-item">
+            <a href="#" class="nav-link">Início</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Contato</a>
+      	</ul>
+		<ul class="navbar-nav ml-auto">
+			<a href="pages/login.php" class="btn btn-block btn-info">Login<a>
+		</ul>
+	  </div>
+    </div>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" style="min-height: 1145.31px;">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0"> Frete Log <small>- Sistema de Gerenciamento de Transporte</small></h1>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+	<div class="card">
+		<div class="card-header" style="text-align: center">
+			<img src="libraries/caminhao.png" alt="FreteLog" style="width: 70%; border-radius: 15px; border: solid black 2px; margin-top: 10px">
+      <h2 style="margin-top: 20px; font-weight: bold">Otimize seu frete, maximize seu lucro.</h2>
+      <div class="card">
+        <h3>
+            Na Frete Log, entendemos que a logística eficiente é a espinha dorsal de qualquer operação bem-sucedida. Com uma equipe experiente e apaixonada por soluções logísticas, estamos aqui para transformar a forma como você gerencia o frete e os transportes da sua empresa.
+        </h3>
+        <h2>
+            O que fazemos:
+        </h2>
+        <h3>
+            Consultoria Personalizada: Avaliamos suas necessidades específicas para oferecer estratégias sob medida que maximizam a eficiência e reduzem custos.
+            Otimização de Processos: Identificamos oportunidades para melhorar suas operações de transporte e logística, garantindo processos mais ágeis e econômicos.
+            Análise de Desempenho: Monitoramos e analisamos continuamente suas operações de frete para fornecer insights valiosos e promover melhorias contínuas.
+            Gestão de Fornecedores: Ajudamos a selecionar e negociar com os melhores fornecedores de transporte, garantindo que você obtenha o melhor serviço pelo melhor preço.
+        </h3>
+        <h2>  
+            Por que escolher a Frete Log?
+        </h2>
+        <h3>
+            Experiência e Expertise: Nossa equipe possui anos de experiência no setor de logística e frete, oferecendo uma visão profunda e soluções eficazes.
+            Soluções Inovadoras: Utilizamos as mais recentes tecnologias e práticas para garantir que sua operação de frete esteja sempre à frente da concorrência.
+            Compromisso com Resultados: Trabalhamos lado a lado com você para alcançar os melhores resultados possíveis, sempre focados em suas necessidades e objetivos.
+            Na Frete Log, estamos comprometidos em tornar seu processo de frete mais eficiente e lucrativo. Entre em contato conosco hoje e descubra como podemos ajudar a transformar a logística do seu negócio.
+            </h3>
+      </div>
+		</div>
+		<div class="card-body">
+
+		</div>
+	</div>
+  </div>
+  <!-- /.content-wrapper -->
+
+<?php include 'includes\component\footer.php' ?> 
+<!-- ./wrapper -->
+
+</body>
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="adminlte/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="adminlte/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="adminlte/dist/js/demo.js"></script>
+
+
+</body>
+
+</html>
