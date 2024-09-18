6135d53acb668ffc17c2481e29b4e0c9a7e92802
Diego Vitali <drochavitali@gmail.com>
2024-09-18 01:12:33 -0300

    Tela inicial ajustada

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Consultoria de Transporte</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+	<link rel="stylesheet" href="adminlte/plugins/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="css/ind.css">
+  
+  
+</head>
+
+<body class="layout-top-nav control-sidebar-slide-open dark-mode layout-navbar-fixed" style="height: auto;">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand-md navbarAjuste">
+    <div class="container">
+      <a href="#" class="navbar-brand">
+        <img src="libraries/Logo FL.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text textoBranco FreteLog">Frete Log</span>
+      </a>
+
+      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav listaNav">
+          <li class="nav-item">
+            <a href="#" class="nav-link textoBranco">Início</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link textoBranco">Contato</a>
+      	</ul>
+		<ul class="navbar-nav ml-auto">
+			<a href="pages/login.php" class="btn btn-block btn-info">Login<a>
+		</ul>
+	  </div>
+    </div>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" style="min-height: 1145.31px;">
+    <div class="content-header">
+      <div class="texto-ini" id="fretelog">
+        <h1 class="m-0"> Frete Log <small>- Sistema de Gerenciamento de Transporte</small></h1>
+      </div>
+      <div class="texto-ini">
+        <h2>Otimize seu frete, maximize seu lucro.</h2>
+        <h2>Na Frete Log, entendemos que a logística eficiente é a espinha dorsal de qualquer operação bem-sucedida. Com uma equipe experiente e apaixonada por soluções logísticas, estamos aqui para transformar a forma como você gerencia o frete e os transportes da sua empresa.</h2>
+      </div>
+      <div class="texto-ini bloco">
+        <h2>Avaliamos suas necessidades específicas para oferecer estratégias sob medida que maximizam a eficiência e reduzem custos.                                             </h2>
+      </div>
+      <div class="texto-ini bloco">
+        <h2>Monitoramos e analisamos continuamente suas operações de frete para fornecer insights valiosos e promover melhorias contínuas.</h2>
+      </div>
+      <br>
+      <br>
+      <div class="texto-ini">
+        <ul class="navbar-nav ml-auto">
+          <a href="pages/login.php" class="btn btn-block btn-info btn-contato">Fale Conosco!<a>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+<?php include 'includes\component\footer.php' ?> 
+<!-- ./wrapper -->
+</body>
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="adminlte/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="adminlte/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="adminlte/dist/js/demo.js"></script>
+
+
+</body>
+
+</html>
